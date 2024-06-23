7b4dc5ea4a0556ddf3976a3e9f921bbc32f8cb43
Stephen Heywood <stephen@ii.coop>
2024-06-24 08:39:59 +1200

    chore: track updates to gatewayapis & implementations

--- apps/web/src/store/index.js
+++ apps/web/src/store/index.js
@@ -476,3 +476,11 @@ export const olderNewEndpoints = derived(
 
 export const implementations = writable([]);
 export const gatewayapis = writable([]);
+
+implementations.subscribe((value) => {
+	console.log("implementations: ", value);
+});
+
+gatewayapis.subscribe((value) => {
+	console.log("gatewayapis: ", value);
+});
