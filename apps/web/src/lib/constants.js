1d928b49b69f36ecd71550988217ef2f78294847
Stephen Heywood <stephen@ii.coop>
2024-07-02 15:37:36 +1200

    fix: update RELEASES_URL env var

--- apps/web/src/lib/constants.js
+++ apps/web/src/lib/constants.js
@@ -4,8 +4,8 @@ export const RELEASES_SITE = __app.env.RELEASE_SITE
 console.log("RELEASES_SITE: ", RELEASES_SITE);
 const head = process.env.HEAD;
 export const RELEASES_URL = head
-  ? `${RELEASES_SITE}/kubernetes-sigs/apisnoop/${head}/resources/coverage`
-  : `${RELEASES_SITE}/kubernetes-sigs/apisnoop/main/resources/coverage`;
+  ? `${RELEASES_SITE}/apisnoop/gatewaysnoop/${head}/resources/coverage`
+  : `${RELEASES_SITE}/apisnoop/gatewaysnoop/main/resources/coverage`;
 console.log("RELEASES_URL: ", RELEASES_URL)
 export const PENDING_ENDPOINTS_URL = `${RELEASES_SITE}/kubernetes/kubernetes/master/test/conformance/testdata/pending_eligible_endpoints.yaml`;
 export const INELIGIBLE_ENDPOINTS_URL = `${RELEASES_SITE}/kubernetes/kubernetes/master/test/conformance/testdata/ineligible_endpoints.yaml`;
